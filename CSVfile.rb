cf1af7686c7bf674e2043392c7cc516fece4c5a9
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2026-04-20 18:18:39 +0200

    Replace homepage_url with url in CSV processing

--- CSVfile.rb
+++ CSVfile.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 csv = CSV.read('_data/reading_list.csv', headers: true)
 csv.each do |row|
   title = row['title']
-  homepage_url = row['homepage_url']
+  url = row['url']
   feed_url = row['feed_url']
 
   # Créer les fichiers markdown dans le dossier _posts_imported
@@ -14,13 +14,13 @@ csv.each do |row|
   content = <<~MARKDOWN
     ---
     title: "#{title}"
-    homepage_url: "#{homepage_url}"
+    url: "#{url}"
     feed_url: "#{feed_url}"
     ---
 
     # #{title}
 
-    Homepage: [#{homepage_url}](#{homepage_url})
+    Homepage: [#{url}](#{url})
     
     Feed: [#{feed_url}](#{feed_url})
   MARKDOWN
